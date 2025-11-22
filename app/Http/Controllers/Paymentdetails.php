c0f6c0ba5ab76b34d18440e342c285c4f39d2bf1
Ali <aliakram4731@gmail.com>
2025-11-22 07:37:47 +0500

    payment info update

--- app/Http/Controllers/Paymentdetails.php
+++ app/Http/Controllers/Paymentdetails.php
@@ -10,28 +10,21 @@ class Paymentdetails extends Controller
 {
     public function success(Request $request)
     {
-        // Convert entire payload to array
         $data = $request->all();
-
-        // Always save raw JSON
         $rawPayload = json_encode($data);
 
-        // APPROVED or DECLINED
         $approved = $data['approved'] ?? false;
         $status = $approved ? 'APPROVED' : 'DECLINED';
 
-        // Extract correct fields based on approval
-        $fields = $approved
-            ? $data['paymentReturnFields']['approveFields'] ?? []
-            : $data['paymentReturnFields']['declineFields'] ?? [];
+        $fields = $data['paymentReturnFields']['approveFields']
+            ?? $data['paymentReturnFields']['declineFields']
+            ?? [];
 
-        // Convert array of name/value pairs into associative array
         $custom = [];
         foreach ($fields as $f) {
             $custom[$f['name']] = $f['value'];
         }
 
-        // Save into DB
         $payment = Transaction::create([
             'name' => $custom['name'] ?? null,
             'comment' => $data['responseFields']['ssl_result_message'] ?? null,
@@ -45,12 +38,12 @@ class Paymentdetails extends Controller
             'raw_payload' => $rawPayload
         ]);
 
-        // Return JSON response
         return response()->json([
             'success' => true,
-            'message' => 'Payment callback processed',
+            'message' => "Callback processed",
             'data' => $payment
         ]);
     }
+
 
 }
