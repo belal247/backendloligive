bffeaa9322cb40eb892e3f5f39fd8a66314a8a72
Ali <aliakram4731@gmail.com>
2025-11-28 15:32:30 +0500

    webhook update

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -66,9 +66,8 @@ class AuthController extends Controller
                     'ssl_pin' => "YXQXYK01X9W2SQ2TX65524D2EMKHRDGQ5KMIH32PV26FTZP7274244JCKBEQYGS3",
                     'ssl_transaction_type' => 'ccsale',
                     'ssl_amount' => $amount,
-                    'ssl_receipt_link_url' => 'https://api.lolligive.com/api/elavon/success',
-                    'ssl_result_format' => 'JSON',
-                    'ssl_get_token' => 'Y'
+                    'ssl_get_token' => 'Y',
+                    'ssl_webhook' => 'https://api.lolligive.com/api/elavon/success'
                 ]);
 
             $token = $response->body(); // ⬅ Elavon returns plain TEXT
